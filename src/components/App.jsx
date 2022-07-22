616e404d5bac2b46f5da0bcc823a3ba51b7103d8
RoRomario360 <fotoromario@gmail.com>
2022-07-22 15:41:07 +0300

    bug-1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -19,6 +19,7 @@ export class App extends Component {
   };
   countTotalFeedback = () => {
     const { good, neutral, bad } = this.state;
+
     return good + neutral + bad;
   };
   countPositivePerCentage = () => {
